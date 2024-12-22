c8398504f8ba0d9440eda9898f80f46a18fadeba
Aru <aru24.m@gmail.com>
2024-12-21 21:42:51 -0500

    Stock Portfolio Management

--- stocks/quotes/urls.py
+++ stocks/quotes/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('About.html', views.about, name="About"),
     path('Stock_data.html', views.show_stock_data, name="show_stock_data"),
     path('Add_stock.html', views.add_stock, name="Add Stock"),
+    path('delete/<stock_id>', views.delete, name="delete"),
+    path('Delete_stock.html', views.delete_stock, name="Delete Stock"),
 ]
